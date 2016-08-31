d3fcca8d1a48dbfda6ec6b6df862e539a66320fb
wgliang <liangcszzu@163.com>
2016-08-31 13:52:36 +0800

    add filter/zeus.go and test

---
+++ filter/zeus/zeus.go
@@ -0,0 +1,47 @@
+package filteraddfield
+
+import (
+	"../../utils"
+	"../../utils/logevent"
+)
+
+const (
+	ModuleName = "add_field"
+)
+
+type FilterConfig struct {
+	utils.FilterConfig
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func DefaultFilterConfig() FilterConfig {
+	return FilterConfig{
+		FilterConfig: utils.FilterConfig{
+			CommonConfig: utils.CommonConfig{
+				Type: ModuleName,
+			},
+		},
+	}
+}
+
+func InitHandler(confraw *utils.ConfigRaw) (retconf utils.TypeFilterConfig, err error) {
+	conf := DefaultFilterConfig()
+	if err = utils.ReflectConfig(confraw, &conf); err != nil {
+		return
+	}
+
+	retconf = &conf
+	return
+}
+
+func (f *FilterConfig) Event(event logevent.LogEvent) logevent.LogEvent {
+	if _, ok := event.Extra[f.Key]; ok {
+		return event
+	}
+	if event.Extra == nil {
+		event.Extra = make(map[string]interface{})
+	}
+	event.Extra[f.Key] = event.Format(f.Value)
+	return event
+}
---
+++ filter/zeus/zeus_test.go
@@ -0,0 +1,26 @@
+package filteraddfield
+
+import (
+	"../../utils/logevent"
+	"testing"
+	"time"
+)
+
+func Test_DefaultFilterConfig(t *testing.T) {
+	DefaultFilterConfig()
+}
+
+func Test_InitHandler(t *testing.T) {
+	// InitHandler(&make(map[string]interface{}))
+}
+
+func Test_Event(t *testing.T) {
+	le := logevent.LogEvent{
+		Timestamp: time.Now(),
+		Message:   "message",
+		Tags:      []string{"frg", "grbhrt"},
+		Extra:     make(map[string]interface{}),
+	}
+	config := DefaultFilterConfig()
+	config.Event(le)
+}
