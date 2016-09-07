646eab88822055e2d8591e60892315bee2b7e328
wgliang <liangcszzu@163.com>
2016-09-07 18:25:43 +0800

    add filter metrics

---
+++ filter/metrics/metrics.go
@@ -0,0 +1,67 @@
+//Filter-plug: metrics
+//metrics is regexp plugin and for filter data you like.
+package metrics
+
+import (
+	"encoding/json"
+	"regexp"
+
+	"github.com/wgliang/logcool/utils"
+)
+
+const (
+	ModuleName = "metrics"
+)
+
+// Define zeus' config.
+type FilterConfig struct {
+	utils.FilterConfig
+	Tag   []string `json:"tag"`
+	Alarm []int64  `json:"alarm"`
+
+	metrics map[string]int64
+}
+
+func init() {
+	utils.RegistFilterHandler(ModuleName, InitHandler)
+}
+
+// Init grok Handler.
+func InitHandler(confraw *utils.ConfigRaw) (tfc utils.TypeFilterConfig, err error) {
+	conf := FilterConfig{
+		FilterConfig: utils.FilterConfig{
+			CommonConfig: utils.CommonConfig{
+				Type: ModuleName,
+			},
+		},
+	}
+	// Reflect config from configraw.
+	if err = utils.ReflectConfig(confraw, &conf); err != nil {
+		return
+	}
+	conf.metrics = make(map[string]int64)
+
+	tfc = &conf
+
+	return
+}
+
+// Filter's event,and this is the main function of filter.
+func (fc *FilterConfig) Event(event utils.LogEvent) utils.LogEvent {
+	if event.Extra == nil {
+		event.Extra = make(map[string]interface{})
+	}
+
+	for _, value := range fc.Tag {
+		re := regexp.MustCompile(value)
+		isv := re.FindString(event.Message)
+		if isv != "" {
+			fc.metrics[isv]++
+		}
+	}
+
+	metrics, _ := json.Marshal(fc.metrics)
+	event.Extra["metrics"] = event.Format(string(metrics))
+
+	return event
+}
---
+++ filter/metrics/metrics_test.go
@@ -0,0 +1,54 @@
+package metrics
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/wgliang/logcool/utils"
+)
+
+func init() {
+	utils.RegistFilterHandler(ModuleName, InitHandler)
+}
+
+func Test_InitHandler(t *testing.T) {
+	config := utils.ConfigRaw{}
+	co, err := InitHandler(&config)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(co)
+}
+
+func Test_Event(t *testing.T) {
+	conf, err := utils.LoadFromString(`{
+		"filter": [{
+			"type": "metrics",
+            "tag":["timeout","refuse"],
+            "alarm":[100,20]
+		}]
+	}`)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	timestamp := time.Now()
+
+	inchan := conf.Get(reflect.TypeOf(make(utils.InChan))).
+		Interface().(utils.InChan)
+
+	outchan := conf.Get(reflect.TypeOf(make(utils.OutChan))).
+		Interface().(utils.OutChan)
+
+	err = conf.RunFilters()
+
+	inchan <- utils.LogEvent{
+		Timestamp: timestamp,
+		Message:   "filter test message",
+	}
+
+	event := <-outchan
+	fmt.Println(event)
+}
